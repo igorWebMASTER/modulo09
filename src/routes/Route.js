67ba7e6aa2d5a2e723252653bbbd8676a67c0309
igorWebMASTER <igor_1917@hotmail.com>
2020-03-15 23:23:03 -0300

    route page added

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -3,33 +3,29 @@ import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
 export default function RouteWrapper({
-   componet: Component,
-   isPrivate = false,
-  ...rest,
- }) {
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
   const signed = false;
 
-  if(!signed && isPrivate){
-    return <Redirect to="/" />
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
   }
 
-  if(signed && !isPrivate){
-    return <Redirect to="/dashboard" />
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
   }
 
-  return (
-    <Route
-    {...res}
-    component={Component}
-    />
-  );
- }
+  return <Route {...rest} component={Component} />;
+}
 
- RouteWrapper.PropTypes = {
-   isPrivate: PropTypes.bool,
-   component: PropTypes.oneoOfType([PropTypes.element, PropTypes.func]).isRequired,
- };
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneoOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
 
- RouteWrapper.defaultProps = {
-   isPrivate: false,
- };
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
